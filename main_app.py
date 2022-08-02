e4bf4443a773390b85200447a17d870e56bbd040
Nicolas301 <110496725+Nicolas301@users.noreply.github.com>
2022-08-02 23:04:05 +0200

    Update main_app.py

--- main_app.py
+++ main_app.py
@@ -3,7 +3,10 @@ import numpy as np
 import matplotlib.pyplot as plt
 
 fig, ax = plt.subplots()
+mensa_data = pd.read_csv('mensa_dump.csv')
+
 ax.scatter(np.random.uniform(0,1,100), np.random.uniform(0,1,100))
 
 st.write('Hier entsteht das DataMining-Projekt MensaCast.')
 st.pyplot(fig)
+st.write(mensa_data.describe())
