e9ac907d1617dde9425782a013cac131870365ce
Nicolas301 <110496725+Nicolas301@users.noreply.github.com>
2022-08-02 22:50:10 +0200

    Update main_app.py

--- main_app.py
+++ main_app.py
@@ -5,5 +5,5 @@ import matplotlib.pyplot as plt
 fig, ax = plt.subplots()
 ax.scatter(np.random.uniform(0,1,100), np.random.uniform(0,1,100))
 
-st.write('Hier entsteht das DataMining-Projekt MensaCast')
+st.write('Hier entsteht das DataMining-Projekt MensaCast.')
 st.pyplot(fig)
