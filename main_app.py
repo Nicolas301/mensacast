3f912550aff419d1715bbf453f6c60b31328e6fb
Nicolas301 <110496725+Nicolas301@users.noreply.github.com>
2022-08-02 23:08:32 +0200

    Update main_app.py

--- main_app.py
+++ main_app.py
@@ -66,10 +66,9 @@ df = df.loc[df['canteen_id']=='1'] # Nur Hauptmensa
 file.close()
 
 fig, ax = plt.subplots()
-mensa_data = pd.read_csv('mensa_dump.csv')
 
 ax.scatter(np.random.uniform(0,1,100), np.random.uniform(0,1,100))
 
 st.write('Hier entsteht das DataMining-Projekt MensaCast.')
 st.pyplot(fig)
-st.write(mensa_data.describe())
+st.write(df.describe())
