a85a300c697ef0da3bfd80d38d0820167021bcd8
Nicolas301 <110496725+Nicolas301@users.noreply.github.com>
2022-08-03 14:07:46 +0200

    Update main_app.py

--- main_app.py
+++ main_app.py
@@ -5,6 +5,24 @@ import seaborn as sns
 import pandas as pd
 from fetcher import *
 
+@st.cache
+def calculate_average_week_prices():
+        beginning_of_week = (pd.Timestamp.today() - pd.DateOffset(days=pd.Timestamp.today().weekday())).date()
+
+        iterated_week = pd.Timestamp(beginning_of_week)
+        avg_prices = []
+        bar_labels = []
+        for i in range(past_weeks+1):
+          df_week = df[(iterated_week<=df['date'])&(df['date']<=(iterated_week+pd.DateOffset(days=5)))]
+          bar_labels.append(f'{iterated_week.strftime("%d.%m.%Y")} - {(iterated_week+pd.DateOffset(days=4)).strftime("%d.%m.%Y")}')
+          if(df_week['date'].size>0):
+            avg_prices.append(np.mean(df_week['price']))
+          else:
+            avg_prices.append(0)
+          iterated_week = iterated_week - pd.DateOffset(days=7)
+
+        return avg_prices, bar_labels
+
 df = fetch_data()
 
 st.write('Hier entsteht das DataMining-Projekt MensaCast.')
@@ -14,19 +32,7 @@ fig, ax = plt.subplots()
 past_weeks = st.slider('Aktuelle Woche und vergangene ... Wochen', min_value = 3, max_value = 51, value = 12, step = 1)
 st.write('Durchschnittspreise:')
 
-beginning_of_week = (pd.Timestamp.today() - pd.DateOffset(days=pd.Timestamp.today().weekday())).date()
-
-iterated_week = pd.Timestamp(beginning_of_week)
-avg_prices = []
-bar_labels = []
-for i in range(past_weeks+1):
-  df_week = df[(iterated_week<=df['date'])&(df['date']<=(iterated_week+pd.DateOffset(days=5)))]
-  bar_labels.append(f'{iterated_week.strftime("%d.%m.%Y")} - {(iterated_week+pd.DateOffset(days=4)).strftime("%d.%m.%Y")}')
-  if(df_week['date'].size>0):
-    avg_prices.append(np.mean(df_week['price']))
-  else:
-    avg_prices.append(0)
-  iterated_week = iterated_week - pd.DateOffset(days=7)
+avg_prices, bar_labels = calculate_average_week_price()
 
 avg_prices.reverse()
 bar_labels.reverse()
