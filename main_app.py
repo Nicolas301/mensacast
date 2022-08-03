6e149bb029e82c869bb2c471349809511d6bcad1
Nicolas301 <110496725+Nicolas301@users.noreply.github.com>
2022-08-03 23:58:06 +0200

    Update main_app.py

--- main_app.py
+++ main_app.py
@@ -52,6 +52,13 @@ st.write('Durchschnittspreise:')
 
 avg_prices, bar_labels = calculate_average_week_prices(monday())
 
+regression = LinearRegression().fit(np.arange(past_weeks+1).reshape(-1,1), avg_prices[(np.size(bar_labels)-past_weeks-1):])
+
+plot_data = pd.DataFrame({'arange_values': np.arange(past_weeks+1),
+                          'avg_prices': avg_prices[(np.size(bar_labels)-past_weeks-1):],
+                          'bar_labels': bar_labels[(np.size(bar_labels)-past_weeks-1):],
+                          'lin_reg_values': regression.predict(np.arange(past_weeks+1).reshape(-1,1))})
+
 sns.barplot(x=bar_labels[(np.size(bar_labels)-past_weeks-1):], y=avg_prices[(np.size(bar_labels)-past_weeks-1):], linewidth = 1, ax = ax)
 sns.lineplot(x=bar_labels[(np.size(bar_labels)-past_weeks-1):], y=avg_prices[(np.size(bar_labels)-past_weeks-1):], ax = ax)
 sns.set_theme(style='whitegrid')
@@ -59,7 +66,6 @@ ax.tick_params(labelsize=7)
 for label in ax.get_xticklabels():
   label.set_rotation(90)
 
-regression = LinearRegression().fit(np.arange(past_weeks+1).reshape(-1,1), avg_prices[(np.size(bar_labels)-past_weeks-1):])
 sns.lineplot(np.arange(past_weeks+1),regression.predict(np.arange(past_weeks+1).reshape(-1,1)), ax = ax, color = '#FF0000')
 ax.yaxis.set_major_formatter(ticker.FormatStrFormatter(f'%.2f €'))
 st.pyplot(fig)
