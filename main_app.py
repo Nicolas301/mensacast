42067093f9a9d3974f6627ed05a978a076118728
Nicolas301 <110496725+Nicolas301@users.noreply.github.com>
2022-08-03 23:30:24 +0200

    Update main_app.py

--- main_app.py
+++ main_app.py
@@ -62,7 +62,7 @@ for label in ax.get_xticklabels():
 regression = LinearRegression().fit(np.arange(past_weeks+1).reshape(-1,1), avg_prices[(np.size(bar_labels)-past_weeks-1):])
 st.write(regression.coef_[0])
 st.write(regression.intercept_)
-sns.lineplot(np.arange(past_weeks+1),regression.predict(np.arange(past_weeks+1).reshape(-1,1)))
+sns.lineplot(np.arange(past_weeks+1),regression.predict(np.arange(past_weeks+1).reshape(-1,1)), ax = ax)
 ax.yaxis.set_major_formatter(ticker.FormatStrFormatter(f'%.2f €'))
 st.pyplot(fig)
 
