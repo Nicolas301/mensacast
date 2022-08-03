ef61b1ef355352346b1ca8bf1007b4a9106b1640
Nicolas301 <110496725+Nicolas301@users.noreply.github.com>
2022-08-03 12:06:34 +0200

    Update main_app.py

--- main_app.py
+++ main_app.py
@@ -22,7 +22,6 @@ bar_labels = []
 for i in range(past_weeks+1):
   df_week = df[(iterated_week<=df['date'])&(df['date']<=(iterated_week+pd.DateOffset(days=5)))]
   bar_labels.append(f'{iterated_week.strftime("%d.%m.%Y")} - {(iterated_week+pd.DateOffset(days=4)).strftime("%d.%m.%Y")}')
-  bar_labels.append(i)
   if(df_week['date'].size>0):
     avg_prices.append(np.mean(df_week['price']))
   else:
