73558e8426bf60afff809c2a3d5c00b4dc39b8a1
Nicolas301 <110496725+Nicolas301@users.noreply.github.com>
2022-08-03 12:00:51 +0200

    Update main_app.py

--- main_app.py
+++ main_app.py
@@ -22,6 +22,7 @@ bar_labels = []
 for i in range(past_weeks+1):
   df_week = df[(iterated_week<=df['date'])&(df['date']<=(iterated_week+pd.DateOffset(days=5)))]
   bar_labels.append(f'{iterated_week.strftime("%d.%m.%Y")} - {(iterated_week+pd.DateOffset(days=4)).strftime("%d.%m.%Y")}')
+  bar_labels.append(i)
   if(df_week['date'].size>0):
     avg_prices.append(np.mean(df_week['price']))
   else:
@@ -32,6 +33,6 @@ avg_prices.reverse()
 bar_labels.reverse()
 
 ax = sns.barplot(x=bar_labels, y=avg_prices)
-for label in ax.get_xticklabels():
-  label.set_rotation(90)
+for bar in ax.containers:
+  ax.bar_label(bar, f'{iterated_week.strftime("%d.%m.%Y")} - {(iterated_week+pd.DateOffset(days=4)).strftime("%d.%m.%Y")}')
 st.pyplot(fig)
