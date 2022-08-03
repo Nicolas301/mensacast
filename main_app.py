609cab6a3ca86bdcb48da702fe25d0455db25392
Nicolas301 <110496725+Nicolas301@users.noreply.github.com>
2022-08-03 23:32:06 +0200

    Update main_app.py

--- main_app.py
+++ main_app.py
@@ -60,9 +60,7 @@ for label in ax.get_xticklabels():
   label.set_rotation(90)
 
 regression = LinearRegression().fit(np.arange(past_weeks+1).reshape(-1,1), avg_prices[(np.size(bar_labels)-past_weeks-1):])
-st.write(regression.coef_[0])
-st.write(regression.intercept_)
-sns.lineplot(np.arange(past_weeks+1),regression.predict(np.arange(past_weeks+1).reshape(-1,1)), ax = ax)
+sns.lineplot(np.arange(past_weeks+1),regression.predict(np.arange(past_weeks+1).reshape(-1,1)), ax = ax, palette = sns.color_palette('#FF0000'))
 ax.yaxis.set_major_formatter(ticker.FormatStrFormatter(f'%.2f €'))
 st.pyplot(fig)
 
