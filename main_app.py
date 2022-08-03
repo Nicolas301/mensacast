292f4c425ceb0dd60873e0544bb16418f71c8945
Nicolas301 <110496725+Nicolas301@users.noreply.github.com>
2022-08-03 14:20:26 +0200

    Update main_app.py

--- main_app.py
+++ main_app.py
@@ -18,6 +18,9 @@ def calculate_average_week_prices(beginning_of_week):
           if(df_week['date'].size>0):
             avg_prices.append(np.mean(df_week['price']))
             bar_labels.append(f'{iterated_week.strftime("%d.%m.%Y")} - {(iterated_week+pd.DateOffset(days=4)).strftime("%d.%m.%Y")}')
+          else:
+            avg_prices.append(0)
+            bar_prices.append('null')
           iterated_week = iterated_week - pd.DateOffset(days=7)
 
         return avg_prices, bar_labels
@@ -33,10 +36,7 @@ st.write('Durchschnittspreise:')
 
 avg_prices, bar_labels = calculate_average_week_prices(monday())
 
-avg_prices.reverse()
-bar_labels.reverse()
-
-ax = sns.barplot(x=bar_labels[:(past_weeks+1)], y=avg_prices[:(past_weeks+1)])
+ax = sns.barplot(x=filter(lambda x: x > 0 ,bar_labels[:(past_weeks+1)]), y=filter(lambda x: x != 'null', avg_prices[:(past_weeks+1)]))
 ax.tick_params(labelsize=7)
 for label in ax.get_xticklabels():
   label.set_rotation(90)
