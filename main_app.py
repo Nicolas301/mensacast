344424b649d4d1649fcd9236352b7f368655e8f5
Nicolas301 <110496725+Nicolas301@users.noreply.github.com>
2022-08-03 19:24:08 +0200

    Update main_app.py

--- main_app.py
+++ main_app.py
@@ -52,7 +52,7 @@ ax.tick_params(labelsize=7)
 for label in ax.get_xticklabels():
   label.set_rotation(90)
 
-ax.yaxis.set_major_formatter(ticker.FormatStrFormatter(f'%0f €'))
+ax.yaxis.set_major_formatter(ticker.FormatStrFormatter(f'%.2f €'))
 st.pyplot(fig)
 
 
