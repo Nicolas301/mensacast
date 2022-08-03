d106a82d05549d3461be2ea8bcf721f5e7b54d9b
Nicolas301 <110496725+Nicolas301@users.noreply.github.com>
2022-08-03 11:10:52 +0200

    Update main_app.py

--- main_app.py
+++ main_app.py
@@ -12,6 +12,8 @@ avg_prices = []
 date_check = []
 
 for date in df['date'].drop_duplicates(inplace = False):
+    if(data<pd.to_datetime('1/1/2021')):
+        continue
     res_df = df.loc[(date-pd.DateOffset(days=30)<=df['date'])&(df['date']<=date)]
     if len(res_df.index) > 0:
         avg_prices.append(np.sum(res_df['price'].astype(float))/len(res_df.index))
@@ -21,6 +23,7 @@ for date in df['date'].drop_duplicates(inplace = False):
 ax.plot(np.arange(len(avg_prices)), avg_prices)
 
 st.write('Hier entsteht das DataMining-Projekt MensaCast.')
+st.write('Monatliche Durchschnittspreise seit 2021'):
 st.pyplot(fig)
 st.write(df.describe())
 st.write('Test')
