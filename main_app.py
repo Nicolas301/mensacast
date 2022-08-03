8852a9f798f07019f298a9999399d6d5fa2940a6
Nicolas301 <110496725+Nicolas301@users.noreply.github.com>
2022-08-03 11:16:44 +0200

    Update main_app.py

--- main_app.py
+++ main_app.py
@@ -11,14 +11,15 @@ fig, ax = plt.subplots()
 avg_prices = []
 date_check = []
 
-for date in df['date'].drop_duplicates(inplace = False):
-    if(date<pd.Timestamp(year = 2021, month = 1, day = 1)):
-        continue
-    res_df = df.loc[(date-pd.DateOffset(days=30)<=df['date'])&(df['date']<=date)]
-    if len(res_df.index) > 0:
-        avg_prices.append(np.sum(res_df['price'].astype(float))/len(res_df.index))
-        date_check.append(date)
-
+#for date in df['date'].drop_duplicates(inplace = False):
+#    if(date<pd.Timestamp(year = 2021, month = 1, day = 1)):
+#        continue
+#    res_df = df.loc[(date-pd.DateOffset(days=30)<=df['date'])&(df['date']<=date)]
+#    if len(res_df.index) > 0:
+#        avg_prices.append(np.sum(res_df['price'].astype(float))/len(res_df.index))
+#        date_check.append(date)
+        
+past_weeks = st.slider('Aktuelle Woche und vergangene ... Wochen', min_value = 4, max_value = 52, value = 12, step = 1)
 
 ax.plot(np.arange(len(avg_prices)), avg_prices)
 
