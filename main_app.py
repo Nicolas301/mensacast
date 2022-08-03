faaf02e139e07a039d7b5786b6f2a3b6d77c4d21
Nicolas301 <110496725+Nicolas301@users.noreply.github.com>
2022-08-03 14:23:31 +0200

    Update main_app.py

--- main_app.py
+++ main_app.py
@@ -8,6 +8,9 @@ from fetcher import *
 def monday():
         return (pd.Timestamp.today() - pd.DateOffset(days=pd.Timestamp.today().weekday())).date()
 
+def avg_filter(p1, p2):
+        return p1[p1!=0], p2[p2!='null']
+
 @st.cache(allow_output_mutation=True)
 def calculate_average_week_prices(beginning_of_week):
         iterated_week = pd.Timestamp(beginning_of_week)
@@ -35,6 +38,7 @@ past_weeks = st.slider('Aktuelle Woche und vergangene ... Wochen', min_value = 3
 st.write('Durchschnittspreise:')
 
 avg_prices, bar_labels = calculate_average_week_prices(monday())
+avg_prices, bar_labels = avg_filter(avg_prices, bar_labels)
 
 ax = sns.barplot(x=bar_labels[:(past_weeks+1)], y=avg_prices[:(past_weeks+1)])
 ax.tick_params(labelsize=7)
