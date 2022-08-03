daf5b531d48aa9d3931674b2331d62212862ac6f
Nicolas301 <110496725+Nicolas301@users.noreply.github.com>
2022-08-03 23:01:17 +0200

    Update fetcher.py

--- fetcher.py
+++ fetcher.py
@@ -2,7 +2,7 @@ import pandas as pd
 import streamlit as st
 
 @st.cache
-def fetch_data():
+def fetch_data(eff_day):
   df = pd.read_csv('mensa_dump.csv', encoding = 'utf-8')
   df.drop_duplicates(subset = ['date', 'meal'], inplace = True) # Mehrfachabfragen raus
   return df
