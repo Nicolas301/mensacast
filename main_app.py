15af1f8f60af7dad3a2df8f4380b055b3883c81c
Nicolas301 <110496725+Nicolas301@users.noreply.github.com>
2022-08-04 22:29:01 +0200

    Update main_app.py

--- main_app.py
+++ main_app.py
@@ -69,9 +69,9 @@ plot_data = pd.DataFrame({'arange_values': np.arange(past_weeks+1),
                           'lin_reg_values': regression.predict(np.arange(past_weeks+1).reshape(-1,1))})
 
 custom_scale = [[0, '#007700'],[.5, '#FFFFAA'],[1, '#AA0000']]
-bar_1 = go.Bar(x = plot_data['bar_labels'], y = plot_data['avg_prices'], marker=dict(color=plot_data['arange_values'], colorscale = custom_scale))
-line_1 = go.Scatter(mode = 'lines', x = plot_data['bar_labels'], y = plot_data['avg_prices'])
-line_2 = go.Scatter(mode = 'lines', x = plot_data['bar_labels'], y = plot_data['lin_reg_values'])
+bar_1 = go.Bar(x = plot_data['bar_labels'], y = plot_data['avg_prices'], marker=dict(color=plot_data['arange_values'], colorscale = custom_scale, showlegend = False))
+line_1 = go.Scatter(mode = 'lines', x = plot_data['bar_labels'], y = plot_data['avg_prices'],line=dict(color="#FFFF00"))
+line_2 = go.Scatter(mode = 'lines', x = plot_data['bar_labels'], y = plot_data['lin_reg_values'],line=dict(color="#FF0000"))
 layout = go.Layout(title="Durchschnittspreise in Euro",title_font_color='#001199')
 fig = go.Figure(data=[bar_1,line_1,line_2], layout=layout)
 st.plotly_chart(fig, use_container_width=True, config=dict(displayModeBar=False))
