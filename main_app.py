38f1fdc119d4ca0d2feed7df25a0f9842d7c83c9
Nicolas301 <110496725+Nicolas301@users.noreply.github.com>
2022-08-04 23:38:01 +0200

    Update main_app.py

--- main_app.py
+++ main_app.py
@@ -77,7 +77,7 @@ with tab1:
         st.plotly_chart(fig, use_container_width=True, config=dict(displayModeBar=False))
 
 with tab2:
-        df_current_week = df[pd.to_timestamp(df['date']) >= monday()]
+        df_current_week = df[pd.Timestamp(df['date']) >= monday()]
         
 with tab3:
         st.write('Dieser Teil der Seite befindet sich noch im Bau!')
