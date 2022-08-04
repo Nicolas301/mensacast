cf9d592d03001901e7d051d33f8c60e8cca04d27
Nicolas301 <110496725+Nicolas301@users.noreply.github.com>
2022-08-04 21:02:56 +0200

    Update main_app.py

--- main_app.py
+++ main_app.py
@@ -51,19 +51,11 @@ st.write('Hier entsteht das DataMining-Projekt MensaCast.')
 
 fig, ax = plt.subplots()
 
-past_weeks = 0
-if 'default_avg_slider_value' in st.session_state:
-        default_avg_slider_value = st.session_state['default_avg_slider_value']
-        if default_avg_slider_value > 23 and mobile_version:
-                default_avg_slider_value = 23
-        past_weeks = st.slider('Aktuelle Woche und vergangene ... Wochen', min_value = 3, max_value = 103-80*mobile_version, value = default_avg_slider_value, step = 1)
-else:
-        past_weeks = st.slider('Aktuelle Woche und vergangene ... Wochen', min_value = 3, max_value = 103-80*mobile_version, value = 11, step = 1)
+past_weeks = st.slider('Aktuelle Woche und vergangene ... Wochen', min_value = 3, max_value = 103-80*mobile_version, value = 11, step = 1)
 if past_weeks == 0:
         past_weeks = 11
 if past_weeks > 23 and mobile_version:
         past_weeks = 23
-st.session_state['default_avg_slider_value'] = past_weeks
         
 st.write('Durchschnittspreise in Euro:')
 
