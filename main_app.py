146ad84600305d1f240b1be7065aab3aa579a63f
Nicolas301 <110496725+Nicolas301@users.noreply.github.com>
2022-08-04 22:20:03 +0200

    Update main_app.py

--- main_app.py
+++ main_app.py
@@ -74,5 +74,5 @@ custom_scale = [[0, '#007700'],[.5, '#FFFFAA'],[1, '#AA0000']]
 bar_1 = go.Bar(x = plot_data['bar_labels'], y = plot_data['avg_prices'], marker=dict(color=plot_data['arange_values'], colorscale = custom_scale))
 line_1 = go.Scatter(mode = 'lines', x = plot_data['bar_labels'], y = plot_data['avg_prices'])
 line_2 = go.Scatter(mode = 'lines', x = plot_data['bar_labels'], y = plot_data['lin_reg_values'])
-fig = go.Figure(data=[bar_1,line_1,line_2])
+fig = go.Figure(data=[bar_1,line_1,line_2],config=dict(displayModeBar=False))
 st.plotly_chart(fig, use_container_width=True)
