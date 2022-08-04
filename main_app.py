b6c7d3ae4a20cbf9f2afe0637f8017733f9debf5
Nicolas301 <110496725+Nicolas301@users.noreply.github.com>
2022-08-04 18:42:06 +0200

    Update main_app.py

--- main_app.py
+++ main_app.py
@@ -44,8 +44,8 @@ df, fetch_date = fetch_data(eff_day)
 st.write(f'Datenstand: {fetch_date.strftime("%d.%m.%Y, %X")}.')
 
 mobile_version = st.checkbox('Mobile Ansicht', value=False, help='Macht Grafiken kleiner, die sich nicht automatisch skalieren.')
-plot_width = 750*(1-mobile_version)+350*mobile_version
-plot_height = 500*(1-mobile_version)+250*mobile_version
+plot_width = 750*(1-mobile_version)+300*mobile_version
+plot_height = 500*(1-mobile_version)+200*mobile_version
 
 st.write('Hier entsteht das DataMining-Projekt MensaCast.')
 
