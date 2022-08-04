a05e7acae159e0a767ec7a01db642f125fcf570f
Nicolas301 <110496725+Nicolas301@users.noreply.github.com>
2022-08-04 22:31:51 +0200

    Update main_app.py

--- main_app.py
+++ main_app.py
@@ -70,8 +70,8 @@ plot_data = pd.DataFrame({'arange_values': np.arange(past_weeks+1),
 
 custom_scale = [[0, '#007700'],[.5, '#FFFFAA'],[1, '#AA0000']]
 bar_1 = go.Bar(x = plot_data['bar_labels'], y = plot_data['avg_prices'], name='', marker=dict(color=plot_data['arange_values'], colorscale = custom_scale), showlegend = False)
-line_1 = go.Scatter(mode = 'lines', x = plot_data['bar_labels'], y = plot_data['avg_prices'],line=dict(color="#FF8800"))
-line_2 = go.Scatter(mode = 'lines', x = plot_data['bar_labels'], y = plot_data['lin_reg_values'],line=dict(color="#FF0000"))
+line_1 = go.Scatter(mode = 'lines', x = plot_data['bar_labels'], y = plot_data['avg_prices'],line=dict(color="#FF8800"), name = 'Durchschnittswerte')
+line_2 = go.Scatter(mode = 'lines', x = plot_data['bar_labels'], y = plot_data['lin_reg_values'],line=dict(color="#FF0000"), name = 'Linearer Trend')
 layout = go.Layout(title="Durchschnittspreise in Euro",title_font_color='#001199')
 fig = go.Figure(data=[bar_1,line_1,line_2], layout=layout)
 st.plotly_chart(fig, use_container_width=True, config=dict(displayModeBar=False))
