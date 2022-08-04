37db80016c7eb6b0e827f825e2b7e9f913e472fb
Nicolas301 <110496725+Nicolas301@users.noreply.github.com>
2022-08-04 23:13:08 +0200

    Update main_app.py

--- main_app.py
+++ main_app.py
@@ -78,7 +78,7 @@ st.plotly_chart(fig, use_container_width=True, config=dict(displayModeBar=False)
 
 experimental = st.checkbox('Experimentellen Plot anzeigen.')
 if experimental:
-        fig2 = px.area(plot_data, x="bar_labels", y="avg_prices", color = "arange_values")
+        fig2 = px.area(plot_data, x="bar_labels", y="avg_prices", color = "arange_values", color_continuous_scale = 'Darkmint')
         st.plotly_chart(fig2, use_container_width=True, config=dict(displayModeBar=False))
 
 
