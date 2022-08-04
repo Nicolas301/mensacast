10a10339c4e12b75dbea31f572328c8342441ec3
Nicolas301 <110496725+Nicolas301@users.noreply.github.com>
2022-08-04 16:13:37 +0200

    Update main_app.py

--- main_app.py
+++ main_app.py
@@ -77,5 +77,5 @@ st.pyplot(fig)
 altair_test_plot = st.checkbox('Experimentellen Altair-Plot anzeigen', help='Diese Funktion befindet sich in Entwicklung und wird die obige Grafik demnächst ersetzen.')
 if altair_test_plot:
         altair_bar = alt.Chart(plot_data).mark_bar().encode(x='bar_labels',y='avg_prices')
-        altair_line = alt.Chart(plot_data).mark_line().encode(x='bar_labels',y='avg_prices',color='yellow')
+        altair_line = alt.Chart(plot_data).mark_line().encode(x='bar_labels',y='avg_prices',color=alt.value('yellow'))
         st.write(altair_bar+altair_line)
