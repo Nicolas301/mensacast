a91aa29f4f1bc3d3cbc4f052c29a1b86521d3341
Nicolas301 <110496725+Nicolas301@users.noreply.github.com>
2022-08-05 10:01:43 +0200

    Update main_app.py

--- main_app.py
+++ main_app.py
@@ -71,6 +71,7 @@ with tab1:
         df_current_day.set_index(np.arange(1,df_current_day.shape[0]+1),inplace=True)
         df_current_day.sort_values(by='Preis', inplace = True)
         df_style = df_current_day.style.format({'Preis': '{:.2f}€'}, decimal = ',').apply(highlight_vegetarian, axis = 1).hide_columns(subset=['Vegetarisch'])
+        df_current_day.drop(columns=['Vegetarisch'], inplace=True)
         st.table(df_style)
 
 
