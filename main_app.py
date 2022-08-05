92ad4634adfb6435efe96f6389dad0c931018291
Nicolas301 <110496725+Nicolas301@users.noreply.github.com>
2022-08-05 22:48:12 +0200

    Update main_app.py

--- main_app.py
+++ main_app.py
@@ -151,8 +151,10 @@ with tab3:
         sel_df = df.iloc[sel_df.index]
         sel_df_past = slice_time(sel_df, effective_day()-pd.DateOffset(months=12), effective_day()-pd.DateOffset(months=3))
         sel_df_present = slice_time(sel_df, effective_day()-pd.DateOffset(months=3))
-        st.write(f'Zahl der in der älteren Vergangenheit gespeicherten Essen mit diesen Komponenten: {sel_df_past.shape[0]}')
-        st.write(f'Zahl der in der jüngeren Vergangenheit gespeicherten Essen mit diesen Komponenten: {sel_df_present.shape[0]}')
+        number_past_days = slice_time(df, effective_day()-pd.DateOffset(months=12), effective_day()-pd.DateOffset(months=3))['date'].unique().shape[0]
+        number_present_days = slice_time(df, effective_day()-pd.DateOffset(months=3))['date'].unique().shape[0]
+        st.write(f'Zahl der in der älteren Vergangenheit gespeicherten Essen mit diesen Komponenten: {sel_df_past.shape[0]}/{number_past_days}')
+        st.write(f'Zahl der in der jüngeren Vergangenheit gespeicherten Essen mit diesen Komponenten: {sel_df_present.shape[0]}/{number_present_days}')
 
 
 with tab4:
