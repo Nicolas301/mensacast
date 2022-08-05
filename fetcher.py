a83abc10fe2fe763391ee1892f274004f0fdc99f
Nicolas301 <110496725+Nicolas301@users.noreply.github.com>
2022-08-05 20:50:21 +0200

    Update fetcher.py

--- fetcher.py
+++ fetcher.py
@@ -5,4 +5,6 @@ import streamlit as st
 def fetch_data(eff_day):
   df = pd.read_csv('mensa_dump.csv', encoding = 'utf-8')
   df.drop_duplicates(subset = ['date', 'meal'], inplace = True) # Mehrfachabfragen raus
+  for remove_multiplequotes in range(3):
+        df['meal'] = df['meal'].str.replace('""','"')
   return df, pd.Timestamp.today(tz='Europe/Berlin')
