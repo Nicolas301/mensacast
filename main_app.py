ba45a602dad94fb635aa4bc6963a757f80fb5a1e
Nicolas301 <110496725+Nicolas301@users.noreply.github.com>
2022-08-05 09:37:17 +0200

    Update main_app.py

--- main_app.py
+++ main_app.py
@@ -65,7 +65,7 @@ with tab1:
         end_of_day = start_of_day + np.timedelta64(1,'D')
         df_current_day = df[(pd.to_datetime(df['date']) >= start_of_day) & (pd.to_datetime(df['date']) < end_of_day)].drop(columns=['id','date']).rename(columns={'meal': 'Essen', 'price': 'Preis', 'is_vegetarian': 'Vegetarisch'})
         df_current_day.set_index(np.arange(1,df_current_day.shape[0]+1),inplace=True)
-        df_style = df_current_day.style.format(precision=2, decimal=',')
+        df_style = df_current_day.style.format('{:,2f} €')
         st.table(df_style)
 
 
