cec8ab9150024c0bc2cda320139fcd6d7f4cebcb
Nicolas301 <110496725+Nicolas301@users.noreply.github.com>
2022-08-05 09:21:36 +0200

    Update main_app.py

--- main_app.py
+++ main_app.py
@@ -65,7 +65,20 @@ with tab1:
         end_of_day = start_of_day + np.timedelta64(1,'D')
         df_current_day = df[(pd.to_datetime(df['date']) >= start_of_day) & (pd.to_datetime(df['date']) < end_of_day)].drop(columns=['id','date']).rename(columns={'meal': 'Essen', 'price': 'Preis', 'is_vegetarian': 'Vegetarisch'})
         st.write(df_current_day)
-        st.markdown('* Test')
+        st.markdown("""<table>
+<thead>
+<tr>
+<th>foo</th>
+<th>bar</th>
+</tr>
+</thead>
+<tbody>
+<tr>
+<td>baz</td>
+<td>bim</td>
+</tr>
+</tbody>
+</table>"""
 
 
 with tab2:
