bf752d12ea54620ed37165449535a41bf06fa0cc
Nicolas301 <110496725+Nicolas301@users.noreply.github.com>
2022-08-06 00:03:26 +0200

    Update main_app.py

--- main_app.py
+++ main_app.py
@@ -30,6 +30,13 @@ def past_day_numbers():
         number_present_days = slice_time(df, effective_day()-pd.DateOffset(months=3))['date'].shape[0]
         return number_past_days, number_present_days
 
+def binom_quantile(n1, n2, X1, X2):
+        p1_hat = X1/n1
+        p2_hat = X2/n2
+        p_hat = (X1+X2)/(n1+n2)
+        z = (p1_hat-p2_hat)/np.sqrt(p_hat*(1-p_hat)*(1/n1+1/n2))
+        return st.norm.cdf(z)
+
 @st.cache(allow_output_mutation=True)
 def calculate_average_week_prices(beginning_of_week):
         iterated_week = pd.Timestamp(beginning_of_week)
@@ -161,6 +168,7 @@ with tab3:
         sel_df_present = slice_time(sel_df, effective_day()-pd.DateOffset(months=3))
         st.write(f'Zahl der in der älteren Vergangenheit gespeicherten Essen mit diesen Komponenten: {sel_df_past.shape[0]}/{number_past_days}')
         st.write(f'Zahl der in der jüngeren Vergangenheit gespeicherten Essen mit diesen Komponenten: {sel_df_present.shape[0]}/{number_present_days}')
+        st.write(f'Quantil: {binom_quantile(sel_df_past, sel_df_present, sel_df_past.shape[0], sel_df_present.shape[0])')
 
 
 with tab4:
