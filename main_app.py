dcc9582165f3d6799960ee27cfeabde0fd89ee3e
Nicolas301 <110496725+Nicolas301@users.noreply.github.com>
2022-08-06 00:13:12 +0200

    Update main_app.py

--- main_app.py
+++ main_app.py
@@ -35,8 +35,8 @@ def binom_quantile(n1, n2, X1, X2):
         p1_hat = X1/n1
         p2_hat = X2/n2
         p_hat = (X1+X2)/(n1+n2)
-        nom = p1_hat-p2_hat
-        denom = np.sqrt(p_hat*(1-p_hat)*(1/n1+1/n2))
+        nom = float(p1_hat-p2_hat)
+        denom = float(np.sqrt(p_hat*(1-p_hat)*(1/n1+1/n2)))
         z = nom/denom
         return sta.norm.cdf(z)
 
