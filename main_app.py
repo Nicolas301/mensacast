04f4785fc05d2b27260ab6843e02518273dd3b77
Nicolas301 <110496725+Nicolas301@users.noreply.github.com>
2022-08-06 00:23:14 +0200

    Update main_app.py

--- main_app.py
+++ main_app.py
@@ -160,7 +160,7 @@ with tab3:
         sel_df = df.iloc[sel_df.index]
         sel_df_past = slice_time(sel_df, effective_day()-pd.DateOffset(months=12), effective_day()-pd.DateOffset(months=3))
         sel_df_present = slice_time(sel_df, effective_day()-pd.DateOffset(months=3))
-        st.metric('Häufigkeit in den letzten drei Monaten', sel_df_present.shape[0], delta=f'{round((sel_df_present.shape[0]/number_present_days)/(sel_df_past.shape[0]/number_past_days)-1)*100,1)} %')
+        st.metric('Häufigkeit in den letzten drei Monaten', sel_df_present.shape[0], delta=f'{round(((sel_df_present.shape[0]/number_present_days)/(sel_df_past.shape[0]/number_past_days)-1)*100,1)} %')
         st.write(f'Zahl der in der älteren Vergangenheit gespeicherten Essen mit diesen Komponenten: {sel_df_past.shape[0]}/{number_past_days}')
         st.write(f'Zahl der in der jüngeren Vergangenheit gespeicherten Essen mit diesen Komponenten: {sel_df_present.shape[0]}/{number_present_days}')
 
