205c5af2deb3dbace771fc6824183f5e5b915396
Nicolas301 <110496725+Nicolas301@users.noreply.github.com>
2022-08-08 17:31:48 +0200

    Update main_app.py

--- main_app.py
+++ main_app.py
@@ -172,11 +172,9 @@ with tab3:
                 st.write('Letzte fünf Gerichte mit dieser Komponentenkombination in den letzten zwölf Monaten:')
                 if sel_df.shape[0] > 5:
                         sel_df = sel_df.iloc[sel_df.shape[0]-5:]
-                sel_df = sel_df.drop(columns=['id']).rename(columns={'date': 'Datum', 'meal': 'Essen', 'price': 'Preis', 'is_vegetarian': 'Vegetarisch'})
+                sel_df = sel_df.drop(columns=['id','is_vegetarian']).rename(columns={'date': 'Datum', 'meal': 'Essen', 'price': 'Preis'})
                 sel_df.set_index(np.arange(1,sel_df.shape[0]+1),inplace=True)
                 sel_df['Datum'] = pd.to_datetime(sel_df['Datum'])
-                sel_df['Vegetarisch'] = sel_df['Vegetarisch'].astype(str)
-                sel_df['Vegetarisch'] = ['Ja' if x == '1' else 'Nein' for x in sel_df['Vegetarisch']]
                 sel_df_style = sel_df.style.format({'Preis': '{:.2f}€', 'Datum': '{:%d.%m.%Y}'}, decimal = ',')
                 st.table(sel_df_style)
 
