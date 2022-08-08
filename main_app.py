4a31265e48eaf8d414de35c62b874a6eff02bbdf
Nicolas301 <110496725+Nicolas301@users.noreply.github.com>
2022-08-08 18:07:29 +0200

    Update main_app.py

--- main_app.py
+++ main_app.py
@@ -184,7 +184,6 @@ with tab4:
         vegetarian_df_present = slice_time(vegetarian_df, effective_day()-pd.DateOffset(months=3))
         veg_share_past = vegetarian_df_past.shape[0]/number_past_days
         veg_share_present = vegetarian_df_present.shape[0]/number_present_days
-        st.write(veg_share_past.shape[0])
+        st.write(veg_share_past)
         st.metric('Anteil vegetarischer Gerichte in den letzten drei Monaten',f'{round(100*veg_share_present,2)} %'.replace('.',','), delta = f'{round(100*(veg_share_present/veg_share_past-1),1)} %'.replace('.',','))
-        # Zahl der Gerichte am Tag
 
