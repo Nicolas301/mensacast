87ef7b648c78f99d423c0ab410ed77998f3a2c25
Nicolas301 <110496725+Nicolas301@users.noreply.github.com>
2022-08-08 17:34:34 +0200

    Update main_app.py

--- main_app.py
+++ main_app.py
@@ -72,7 +72,7 @@ with tab1:
         weekday_labels.append(f'Donnerstag, {(pd.to_datetime(monday()) + np.timedelta64(3,"D")).strftime("%d.%m.%Y")}')
         weekday_labels.append(f'Freitag, {(pd.to_datetime(monday()) + np.timedelta64(4,"D")).strftime("%d.%m.%Y")}')
         selected_weekday = st.selectbox('Wochentag', weekday_labels,index=min(today_index,4))
-        highlight_veg = st.checkbox('Vegetarische Gerichte hervorheben', value = False, help = 'In seltenen Fällen werden Gerichte vom Studierendenwerk fälschlich als vegetarisch oder nicht-vegetarisch eingeordnet. Dies ist kein Fehler dieser Webseite.')
+        highlight_veg = st.checkbox('Vegetarische Gerichte hervorheben', value = False, help = 'In einigen Fällen werden Gerichte vom Studierendenwerk fälschlich als vegetarisch oder nicht-vegetarisch eingeordnet. Dies ist kein Fehler dieser Webseite.')
         start_of_day = pd.to_datetime(monday())
         if selected_weekday == weekday_labels[1]:
                 start_of_day = pd.to_datetime(monday()) + np.timedelta64(1,'D')
