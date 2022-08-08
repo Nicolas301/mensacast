a9f8e3a9a01bbdec6bcecda5602c67edfb3872ed
Nicolas301 <110496725+Nicolas301@users.noreply.github.com>
2022-08-08 18:37:34 +0200

    Update main_app.py

--- main_app.py
+++ main_app.py
@@ -183,4 +183,6 @@ with tab4:
         sel_df_past = slice_time(df, effective_day()-pd.DateOffset(months=12), effective_day()-pd.DateOffset(months=3))
         sel_df_present = slice_time(df, effective_day()-pd.DateOffset(months=3))
         avg_present = sel_df_present.shape[0]/pd.unique(sel_df_present['date']).shape[0]
+        st.write(sel_df_present.shape[0])
+        st.write(pd.unique(sel_df_present['date']).shape[0])
         st.metric('Durchschnittliche Zahl der Gerichte', str(round(avg_present,1)).replace('.',','), delta = f'{round(100*(3*sel_df_present.shape[0]/sel_df_past.shape[0]-1),1)} %'.replace('.',','))
