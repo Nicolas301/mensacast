f56b16f5c36c681765819b3eef786696a073bf7e
Nicolas301 <110496725+Nicolas301@users.noreply.github.com>
2022-08-18 21:41:14 +0200

    Update main_app.py

--- main_app.py
+++ main_app.py
@@ -192,7 +192,7 @@ with tab5:
         clog_list = []
         for build_nr in build_df['Buildnummer']:
                 clog_string = ''
-                for clog_line in clograw_df[clograw_df['Buildnummer'] = build_nr]['Change']:
+                for clog_line in clograw_df[clograw_df['Buildnummer'] == build_nr]['Change']:
                         clog_string = clog_string + '- ' + clog_line + '\n'
                 clog_list.append(clog_string)
         clog_df = pd.DataFrame(data={'Buildnummer': build_df['Buildnummer'], 'Datum': build_df['Datum'], 'Änderungen': clog_list})
