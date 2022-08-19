ce2b77305908d19ee65edb164a718819572663bd
Nicolas301 <110496725+Nicolas301@users.noreply.github.com>
2022-08-19 09:33:40 +0200

    Update main_app.py

--- main_app.py
+++ main_app.py
@@ -249,7 +249,7 @@ with tab5:
                         nonveg_prop = nonveg_count/nonveg_df.shape[0]
                         veg_prop = veg_count/veg_df.shape[0]
                         prop_dict[noun] = (nonveg_prop, veg_prop)
-                        if (nonveg_prop >= 1.5*veg_prop) and (veg_count+nonveg_count >= 3):
+                        if (nonveg_prop >= 2.5*veg_prop) and (veg_count+nonveg_count >= 3):
                                 st.write(noun+' -- '+str(veg_prop)+' -- '+str(nonveg_prop))
                         
                         
