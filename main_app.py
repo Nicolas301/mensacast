44b4dd332601a8590157719bdbfbb7e983145600
Nicolas301 <110496725+Nicolas301@users.noreply.github.com>
2022-08-19 09:43:22 +0200

    Update main_app.py

--- main_app.py
+++ main_app.py
@@ -246,7 +246,7 @@ with tab5:
                 nonveg_component_list = []
                 for noun in noun_list:
                         veg_count = veg_df.loc[[noun in veg_meal for veg_meal in veg_df]].shape[0]
-                        veg_prop = veg_count/veg_df.shape[0]
+                        nonveg_count = nonveg_df.loc[[noun in nonveg_meal for nonveg_meal in nonveg_df]].shape[0]
                         prop_dict[noun] = (nonveg_count, veg_count)
                         if veg_prop == 0 and nonveg_prop >= 5:
                                 st.write(noun+' -- '+str(veg_count)+' -- '+str(nonveg_count))
